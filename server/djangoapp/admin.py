c7b54cf5bd0e579f04b2033aa8e6c6f0e57b716b
MarioHdzFls <94874845+MarioHdzFls@users.noreply.github.com>
2026-02-24 23:35:21 -0600

    Register CarMake and CarModel in admin site

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 # Permitir editar modelos de coche directamente en la página de la marca
 class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1
 
+
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
     list_display = ('name', 'country_of_origin')
+
 
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
     list_filter = ['type', 'car_make', 'year']
     search_fields = ['name', 'car_make__name']
+
 
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
